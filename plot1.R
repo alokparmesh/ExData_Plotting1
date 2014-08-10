714713a3c05c70dcf8759b94dafdba63013d3981
alokparmesh <alokparmesh@gmail.com>
2014-08-10 16:18:21 -0700

    Commiting the plot1 code and png

---
+++ plot1.R
@@ -0,0 +1,8 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",destfile="assign1.zip")
+unzip("assign1.zip") 
+dat<- read.table('household_power_consumption.txt',header=TRUE,sep=";",na.strings="?")
+dat$Date <- as.Date(dat$Date,"%d/%m/%Y")
+dat2 <- dat[dat$Date >= as.Date('2007-02-01') & dat$Date <= as.Date('2007-02-02'),]
+png(filename = "plot1.png",width = 480, height = 480, units = "px")
+hist(dat2$Global_active_power,main="Global Active Power",xlab="Global Active Power (kilowatts)",col="Red")
+dev.off()
